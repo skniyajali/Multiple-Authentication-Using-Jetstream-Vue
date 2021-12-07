b3cd5958a05401ddbddb1fa4f0242c7f6638e62a
Sk Niyaj Ali <26448068+niyajali@users.noreply.github.com>
2021-12-07 22:22:42 +0530

    final updates

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -26,8 +26,8 @@ class AdminFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            // 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'password' => '$2a$10$S77gIufmlV7D79KW.dOAiOAozaM.1q2GMgbd7UN9Og5kgbcNTBesS', // Pass@123
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            // 'password' => '$2a$10$S77gIufmlV7D79KW.dOAiOAozaM.1q2GMgbd7UN9Og5kgbcNTBesS', // Pass@123
             'remember_token' => Str::random(10),
         ];
     }
